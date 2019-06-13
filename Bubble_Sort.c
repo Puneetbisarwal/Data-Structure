3433b4e4949652d193a8e08e138a6f28b551823f
Puneetbisarwal <44027672+Puneetbisarwal@users.noreply.github.com>
2019-06-14 02:22:05 +0530

    Add files via upload

---
+++ Bubble_Sort.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+void swap(int *first, int *second)
+{
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+
+}
+
+
+int main()
+{
+
+int n,i,j,temp,arr[1000];
+printf("\n---Bubble Sorting---\n\n\n");
+
+printf("Enter the Number of Elements = ");
+scanf("%d",&n);
+
+printf("Enter %d element \n",n);
+
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+
+for(i=0;i<n-1;i++)
+{
+int flag=0;
+
+for(j=0;j<n-i-1;j++)
+{
+if(arr[j]>arr[j+1])
+{
+
+swap(&arr[j], &arr[j+1]);
+flag=1;
+}
+}
+
+if(flag==0)
+break;
+}
+
+printf("Sorted list in Ascending order :\n");
+
+for(i=0;i<n;i++)
+printf("%d\n",arr[i]);
+
+}
+
